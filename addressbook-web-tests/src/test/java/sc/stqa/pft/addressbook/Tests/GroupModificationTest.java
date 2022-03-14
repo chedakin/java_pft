e6f423f28c294208521057d6cc18fc97d4dd602a
chedakin <101333091+chedakin@users.noreply.github.com>
2022-03-14 14:21:38 +0300

    Тест на редактирование группы добавлен

---
+++ addressbook-web-tests/src/test/java/sc/stqa/pft/addressbook/Tests/GroupModificationTest.java
@@ -0,0 +1,17 @@
+package sc.stqa.pft.addressbook.Tests;
+
+import org.testng.annotations.Test;
+import sc.stqa.pft.addressbook.models.GroupData;
+
+public class GroupModificationTest extends TestBase{
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test2", "test2.2", "test2.3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
--- addressbook-web-tests/src/test/java/sc/stqa/pft/addressbook/appmanager/ApplicationManger.java
+++ addressbook-web-tests/src/test/java/sc/stqa/pft/addressbook/appmanager/ApplicationManger.java
@@ -15,7 +15,7 @@ public class ApplicationManger {
 
     public WebDriver driver;
     public String baseUrl;
-    public boolean acceptNextAlert = true;
+
     public StringBuffer verificationErrors = new StringBuffer();
 
     public void init() {
@@ -40,38 +40,11 @@ public class ApplicationManger {
         }
     }
 
-    public boolean isElementPresent(By by) {
-      try {
-        driver.findElement(by);
-        return true;
-      } catch (NoSuchElementException e) {
-        return false;
-      }
-    }
 
-    public boolean isAlertPresent() {
-      try {
-        driver.switchTo().alert();
-        return true;
-      } catch (NoAlertPresentException e) {
-        return false;
-      }
-    }
 
-    public String closeAlertAndGetItsText() {
-      try {
-        Alert alert = driver.switchTo().alert();
-        String alertText = alert.getText();
-        if (acceptNextAlert) {
-          alert.accept();
-        } else {
-          alert.dismiss();
-        }
-        return alertText;
-      } finally {
-        acceptNextAlert = true;
-      }
-    }
+
+
+
 
     public GroupHelper getGroupHelper() {
         return groupHelper;
