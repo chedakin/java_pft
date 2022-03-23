163d2bff11f8beb06cfd33d8f74346475dc2422b
chedakin <101333091+chedakin@users.noreply.github.com>
2022-03-23 18:58:22 +0300

    провайдер csv работает провайдер xml не хочет работать

--- addressbook-web-tests/src/test/java/sc/stqa/pft/addressbook/Tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/sc/stqa/pft/addressbook/Tests/GroupCreationTest.java
@@ -1,21 +1,56 @@
 package sc.stqa.pft.addressbook.Tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import sc.stqa.pft.addressbook.models.GroupData;
 import sc.stqa.pft.addressbook.models.Groups;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase{
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromCSV() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src\\test\\resources\\groups.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
 
+// НЕ РАБОТАЕТ!!!!
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromXML() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src\\test\\resources\\groups.xml")));
+
+    StringBuilder xmlBuilder = new StringBuilder();
+    String line = reader.readLine();
+    while (line != null) {
+      xmlBuilder.append(line);
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    List<GroupData[]> groups = (List<GroupData[]>) xstream.fromXML(xmlBuilder.toString());
+    return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validGroupsFromCSV")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
 
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("group4");
     app.group().create(group);
 
     assertThat(app.group().count(), equalTo(before.size()+1));
@@ -25,7 +60,7 @@ public class GroupCreationTest extends TestBase{
 
   }
 
-  @Test
+  @Test(enabled = false)
   public void testBadGroupCreation() throws Exception {
 
     app.goTo().groupPage();
