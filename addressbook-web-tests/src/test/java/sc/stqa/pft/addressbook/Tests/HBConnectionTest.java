cf4f3accfbd331bc6ccc9c15ab0a6eaaa4d592a3
chedakin <101333091+chedakin@users.noreply.github.com>
2022-03-30 19:16:41 +0300

    Установления соединения с базой данных Hibernate примеры

---
+++ addressbook-web-tests/src/test/java/sc/stqa/pft/addressbook/Tests/HBConnectionTest.java
@@ -0,0 +1,59 @@
+package sc.stqa.pft.addressbook.Tests;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import sc.stqa.pft.addressbook.models.ContactData;
+import sc.stqa.pft.addressbook.models.GroupData;
+
+import java.util.List;
+
+public class HBConnectionTest {
+
+    private SessionFactory sessionFactory;
+
+    @BeforeClass
+    protected void setUp() throws Exception {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy( registry );
+        }
+    }
+
+    @Test(enabled = false)
+    public void testHBConnectionGroup() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<GroupData> result = session.createQuery( "from GroupData" ).list();
+        for ( GroupData group : result ) {
+            System.out.println( group );
+        }
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Test
+    public void testHBConnectionContact() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<ContactData> result = session.createQuery( "from ContactData where deprecated = '000-00-00' " ).list();
+        for ( ContactData contact : result ) {
+            System.out.println( contact );
+        }
+        session.getTransaction().commit();
+        session.close();
+    }
+}
--- addressbook-web-tests/src/test/java/sc/stqa/pft/addressbook/models/ContactData.java
+++ addressbook-web-tests/src/test/java/sc/stqa/pft/addressbook/models/ContactData.java
@@ -4,41 +4,73 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
     @XStreamOmitField
     @JsonIgnore
+    @Id
+    @Column(name = "id")
     private int id = Integer.MAX_VALUE;
+
     @Expose
+    @Column(name = "firstname")
     private String firstname;
+
     @Expose
+    @Column(name = "lastname")
     private String lastname;
+
     @Expose
+    @Column(name = "email")
+    @Type(type = "text")
     private String email;
+
     @Expose
+    @Column(name = "address")
+    @Type(type = "text")
     private String address;
+
     @Expose
+    @Column(name = "mobile")
+    @Type(type = "text")
     private String mobile;
+
     @XStreamOmitField
     @JsonIgnore
+    @Transient
     private String group;
+
     @Expose
+    @Column(name = "home")
+    @Type(type = "text")
     private String homePhone;
+
     @Expose
+    @Column(name = "work")
+    @Type(type = "text")
     private String workPhone;
+
     @XStreamOmitField
     @JsonIgnore
+    @Transient
     private String allPhones;
+
     @XStreamOmitField
     @JsonIgnore
-    private File photo;
+    @Column(name = "photo")
+    @Type(type = "text")
+    private String photo;
 
     public File getPhoto() {
-        return photo;
+        return new File(photo);
     }
     public String getAllPhones() {
         return allPhones;
@@ -113,7 +145,7 @@ public class ContactData {
     }
 
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
 
