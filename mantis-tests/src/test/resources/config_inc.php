092d8da38eb9ec6a3a9a638c43591e625b86ca7c
chedakin <101333091+chedakin@users.noreply.github.com>
2022-04-07 14:39:08 +0300

    ftp в мантис. Отключаем каптчу

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_signup_use_captcha     = OFF;
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'vqSTlB4CKH/ucx8GEj52ElfO5mg2f4yPHc5r1AbXs2U=';
