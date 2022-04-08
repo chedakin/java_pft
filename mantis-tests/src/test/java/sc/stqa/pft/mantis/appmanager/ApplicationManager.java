83e12e221f5781eaeee41068a04151bd46de5d6b
chedakin <101333091+chedakin@users.noreply.github.com>
2022-04-08 15:54:48 +0300

    Внешний мейл сервер. Сервер не запускается, поэтому тест не исправлен. Но помошник сделан.

--- mantis-tests/src/test/java/sc/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/sc/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -24,6 +24,7 @@ public class ApplicationManager {
     private RegistrationHelper registrationHelper;
     private FtpHelper ftp;
     private MailHelper mailHelper;
+    private JamesHelper jamesHelper;
 
 
     public ApplicationManager(String browser) {
@@ -79,6 +80,13 @@ public class ApplicationManager {
             mailHelper = new MailHelper(this);
         }
         return mailHelper;
+    }
+
+    public JamesHelper james() {
+        if(jamesHelper == null) {
+            jamesHelper = new JamesHelper(this);
+        }
+        return jamesHelper;
     }
 
     public WebDriver getDriver() {
